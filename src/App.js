fabfb3ae362ed18661c4897ca752e1876c14c07b
hangil-jung <hangil.jung@samsung.com>
2020-12-28 15:13:03 +0900

    codeeditor instance added. simple code mirror api punch holed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-// import Button from './Button';
-import CodeMirror from '@uiw/react-codemirror';
-import 'codemirror/keymap/sublime';
-import 'codemirror/theme/monokai.css';
+import CodeEditor from './CodeEditor';
 
-const code = '#include <bits/stdc++.h>\nusing namespace std;\nint main() {\n\tputs("Hello world");\n\treturn 0;\n}';
 
 function App() {
   return (
     <div className="App">
       <div className="Code-Edit">
-        <CodeMirror
-          value={code}
-          options={{
-            theme: 'monokai',
-            keyMap: 'sublime',
-            mode: 'cpp'
-          }}
-          height="100vh"
-          width="100vw"
-        />
+        <CodeEditor />
 
       </div>
     </div>
---
+++ src/CodeEditor.js
@@ -0,0 +1,52 @@
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+import React from 'react';
+
+const defaultCode = '#include <bits/stdc++.h>\nusing namespace std;\nint main() {\n\tputs("Hello world");\n\treturn 0;\n}';
+
+class CodeEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        //For debug usage
+        window.codeEditor = this;
+        this.state = {
+            code: defaultCode
+        }
+    }
+    renderCodeMirror() {
+        return (
+            <CodeMirror
+                value={this.state.code}
+                options={{
+                    theme: 'monokai',
+                    keyMap: 'sublime',
+                    mode: 'cpp'
+                }}
+                height="100vh"
+                width="100vw"
+                onChange={(editor) => {
+                    const newCode = editor.getValue();
+                    this.updateCode(newCode);
+                }}
+            />
+        )
+    }
+    render() {
+        return (
+            <div className="Code-Editor">
+                {this.renderCodeMirror()}
+            </div>
+        )
+    }
+    updateCode(newCode) {
+        this.setState({
+            code: newCode
+        })
+    }
+    getCode() {
+        return this.state.code;
+    }
+}
+
+export default CodeEditor
