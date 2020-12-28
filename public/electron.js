79049b48db66e1f36ed1fcf17c166f11d0282127
Einstrasse <hg9587@naver.com>
2020-12-28 17:25:23 +0900

    loading template cdoe function complete

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,14 @@
 const electron = require("electron");
+const fs = require('fs');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const Menu = electron.Menu;
 const isDev = require("electron-is-dev");
+const { MenuItem } = require("electron");
 const isMac = process.platform === 'darwin'
 let mainWindow;
+/*
 let menuTemplate = [
     {
         label: "File",
@@ -21,12 +24,6 @@ let menuTemplate = [
             }
         ]
     },
-    // {
-    //     label: "Templates",
-    //     submenu: [
-    //         label
-    //     ]
-    // },
     {
         label: "Test",
         submenu: [
@@ -51,7 +48,55 @@ let menuTemplate = [
     }
   ]
 Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
-
+*/
+(function setMenu() {
+    const menu = new Menu()
+    menu.append(new MenuItem({ 
+        id: '1',
+        label: 'File', 
+        submenu: [
+            {
+                role: isMac ? "close" : "quit"
+            }
+        ]
+    }))
+    let templates = []
+    let files = fs.readdirSync(path.join(__dirname, "templates"));
+    files.forEach(file_name => {
+        let file_data = fs.readFileSync(path.join(__dirname, "templates", file_name));
+        let encoded = Buffer.from(file_data).toString('base64');
+        let jscode = 'window.CodeEditor.updateCode(atob("' + encoded + '"));';
+        // console.log(file_data)
+        // console.log(encoded)
+        // console.log(jscode)
+        templates.push({
+            label: file_name,
+            click: () => {
+                mainWindow.webContents.executeJavaScript(jscode);
+            }
+        })
+    })
+    console.log(templates);
+    menu.append(new MenuItem({
+        id: '2',
+        label: "Templates",
+        submenu: templates
+    }))
+    menu.append(new MenuItem({ 
+        id: '3',
+        role: "help",
+        submenu: [
+            {
+                label: "About",
+                click: async() => {
+                    await electron.shell.openExternal("https://github.com/Einstrasse/aps-studio")
+                }
+            }
+        ]
+    }))
+    
+    Menu.setApplicationMenu(menu);
+})();
 
 function createWindow() {
     mainWindow = new BrowserWindow({ width: 900, height: 680 });
---
+++ public/templates/codeforce.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+void solve() {
+
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while(tc--) solve();
+    return 0;
+}
