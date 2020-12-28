eed3b24e8186ff308af2f6510bf10f48dad29eb7
hangil-jung <hangil.jung@samsung.com>
2020-12-28 13:47:04 +0900

    codemirror added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from './Button';
-import CodeMirror from './CodeMirror';
+// import Button from './Button';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+
+const code = '#include <bits/stdc++.h>\nusing namespace std;\nint main() {\n\tputs("Hello world");\n\treturn 0;\n}';
 
 function App() {
   return (
     <div className="App">
-      <CodeMirror />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Code-Edit">
+        <CodeMirror
+          value={code}
+          options={{
+            theme: 'monokai',
+            keyMap: 'sublime',
+            mode: 'cpp'
+          }}
+          height="100vh"
+          width="100vw"
+        />
+
+      </div>
     </div>
   );
 }
