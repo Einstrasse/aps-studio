2f3e8056897b7d4fe492e555d5a31c1697fd72e8
Einstrasse <hg9587@naver.com>
2020-12-28 20:53:12 +0900

    resizable layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import CodeEditor from './CodeEditor';
 import Toolbar from './Toolbar';
-
+import { Resize, ResizeVertical, ResizeHorizon } from "react-resize-layout";
 
 function App() {
   return (
     <div className="App">
       <div className="VerticalContainer">
+        <Toolbar />
+        <hr />
+        <Resize handleWidth="3px" handleColor="#30f">
+          <ResizeVertical height="100%" className="resizable-code-panel">
+            <div className="CodePanel panel">
+              <CodeEditor varname="CodeEditor" mode="cpp" />
+            </div>  
+          </ResizeVertical>
+          <ResizeVertical minHeight="150px" className="resizable-input-panel">
+            <div className="inputPanel panel">
+              <CodeEditor varname="InputEditor" mode="txt" />
+            </div>
+          </ResizeVertical>
+        </Resize>
+      </div>
+      {/* <div className="VerticalContainer">
         <Toolbar />
         <div className="CodePanel">
           <CodeEditor varname="CodeEditor" mode="cpp" />
@@ -15,7 +31,7 @@ function App() {
         <div className="footer">
           <CodeEditor varname="InputEditor" mode="txt" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
