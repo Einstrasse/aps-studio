55739e5c4d9ee391b0f8c80d2d09237a8c33932b
Einstrasse <hg9587@naver.com>
2020-12-29 17:08:30 +0900

    somehow compilation and run function.

--- public/electron.js
+++ public/electron.js
@@ -6,49 +6,66 @@ const path = require("path");
 const Menu = electron.Menu;
 const isDev = require("electron-is-dev");
 const { MenuItem } = require("electron");
+const os = require('os');
+const { exec, execFile } = require('child_process');
 const isMac = process.platform === 'darwin'
 let mainWindow;
-/*
-let menuTemplate = [
-    {
-        label: "File",
-        submenu: [
-            {
-                label: "Open File"
-            },
-            {
-                label: "Save"
-            },
-            {
-                role: isMac ? "close" : "quit"
-            }
-        ]
-    },
-    {
-        label: "Test",
-        submenu: [
-            {
-                label: "Tempalte code 1",
-                click: async() => {
-                    mainWindow.webContents.executeJavaScript('window.CodeEditor.updateCode("Template code 1");');
-                }
-            }
-        ]
-    },
-    {
-        role: "help",
-        submenu: [
-            {
-                label: "About",
-                click: async() => {
-                    await electron.shell.openExternal("https://github.com/Einstrasse/aps-studio")
-                }
-            }
-        ]
+function execJsCode(jscode) {
+    let ret;
+    try {
+        ret = mainWindow.webContents.executeJavaScript(jscode).then((d) => {return d});
+    } catch(e) {
+        console.log('execJsCode', e);
+        throw e;
     }
-  ]
-Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
-*/
+    return ret;
+}
+function writeText(varname, data) {
+    let encoded = Buffer.from(data).toString('base64');
+    let jscode = `window.${varname}.updateCode(atob("${encoded}"));`;
+    //mainWindow.webContents.executeJavaScript(jscode);
+    execJsCode(jscode);
+}
+async function readText(varname) {
+    //returns promise object
+    let jscode = `window.${varname}.getCode()`
+    let ret = await execJsCode(jscode);
+    return ret;
+    // return await mainWindow.webContents.executeJavaScript(jscode);
+}
+function buildAndRun(code, input) {
+    const code_file = path.join(os.tmpdir(), "code.cpp")
+    const out_file = path.join(os.tmpdir(), "code.out")
+    const input_file = path.join(os.tmpdir(), "input.txt")
+    fs.writeFileSync(code_file, code);
+    fs.writeFileSync(input_file, input);
+    console.log(process.platform)
+    console.log(code_file)
+    exec(`g++ ${code_file} -o ${out_file} -std=c++11 -Wall`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(error);
+            writeText('LogView', stderr);
+            return;
+        }
+        console.log('stdout', stdout);
+        console.log('stderr', stderr);
+        writeText('LogView', [stdout, stderr].join('\n'));
+        execFile(out_file, ` < ${input_file}`.split(' '), (error, stdout, stderr) => {
+            if (error) {
+                console.log(error);
+                writeText('LogView', stderr);
+                return;
+            }
+            console.log('stdout', stdout);
+            console.log('stderr', stderr);
+            writeText('LogView', [stdout, stderr].join('\n'));
+        })
+        
+    });
+
+}
+
+
 (function setMenu() {
     const menu = new Menu()
     menu.append(new MenuItem({ 
@@ -64,26 +81,36 @@ Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
     let files = fs.readdirSync(path.join(__dirname, "templates"));
     files.forEach(file_name => {
         let file_data = fs.readFileSync(path.join(__dirname, "templates", file_name));
-        let encoded = Buffer.from(file_data).toString('base64');
-        let jscode = 'window.CodeEditor.updateCode(atob("' + encoded + '"));';
-        // console.log(file_data)
-        // console.log(encoded)
-        // console.log(jscode)
         templates.push({
             label: file_name,
             click: () => {
-                mainWindow.webContents.executeJavaScript(jscode);
+                writeText('CodeEditor', file_data);
             }
         })
     })
-    console.log(templates);
     menu.append(new MenuItem({
         id: '2',
         label: "Templates",
         submenu: templates
     }))
-    menu.append(new MenuItem({ 
+    menu.append(new MenuItem({
         id: '3',
+        label: "Build",
+        submenu: [
+            {
+                label: "Run with Input (Ctrl + F5)",
+                accelerator: "Ctrl+F5",
+                click: async() => {
+                    console.log("Run with Input!");
+                    var code = await readText("CodeEditor");
+                    var input = await readText("InputEditor");
+                    buildAndRun(code, input);
+                }
+            }
+        ]
+    }))
+    menu.append(new MenuItem({ 
+        id: '4',
         role: "help",
         submenu: [
             {
--- src/App.js
+++ src/App.js
@@ -39,36 +39,11 @@ function App() {
         </div>
         <div key="LogPanel" className="grid-box">
           <div className="grid-label">
-            Texxxxxt
+            Log
           </div>
           <CodeEditor varname="LogView" mode="txt" />  
         </div>
       </GridLayout>      
-      {/* <Resize handleWidth="3px" handleColor="#30f">
-        <ResizeVertical height="20px" minHeight="20px">
-          <Toolbar />
-          <hr />
-        </ResizeVertical>
-        <ResizeVertical height="100%" className="resizable-code-panel">
-        <Resize handleColor="red" handleWidht="8px">
-          <ResizeHorizon width="400px">
-            <div className="CodePanel panel">
-              <CodeEditor varname="CodeEditor" mode="cpp" />
-            </div>  
-          </ResizeHorizon>
-          <ResizeHorizon minWidth="100px">
-          <div className="CodePanel panel">
-              <CodeEditor varname="InputEditor" mode="txt" />
-            </div>  
-          </ResizeHorizon>
-        </Resize>
-        </ResizeVertical>
-        <ResizeVertical minHeight="150px" className="resizable-input-panel">
-          <div className="inputPanel panel">
-            <CodeEditor varname="LogView" mode="txt" />
-          </div>
-        </ResizeVertical>
-      </Resize> */}
     </div>
   );
 }
